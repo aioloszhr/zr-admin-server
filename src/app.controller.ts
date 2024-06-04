42e82016c9dcd5cc91fa7b877188d5708704723f
zhangrui <aioloszh092@163.com>
2024-06-04 17:24:29 +0800

    feat: 通过jwt、typeorm、mysql实现注册登录功能

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,57 +1,25 @@
-import {
-	Controller,
-	Get,
-	Inject,
-	Res,
-	Session,
-	Headers,
-	UnauthorizedException
-} from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { JwtService } from '@nestjs/jwt';
-
-import type { Response } from 'express';
+import { LoginGuard } from './login.guard';
 
 @Controller()
 export class AppController {
 	constructor(private readonly appService: AppService) {}
 
-	@Inject(JwtService)
-	private jwtService: JwtService;
-
 	@Get('sss')
-	getHello(@Session() session): number {
-		console.log(session);
-		session.count = session.count ? session.count + 1 : 1;
-		return session.count;
+	getHello(): string {
+		return 'Hello World!';
 	}
 
-	@Get('ttt')
-	ttt(
-		@Headers('authorization') authorization: string,
-		@Res({ passthrough: true }) response: Response
-	) {
-		if (authorization) {
-			try {
-				const token = authorization.split(' ')[1];
-				const data = this.jwtService.verify(token);
+	@Get('aaa')
+	@UseGuards(LoginGuard)
+	aaa() {
+		return 'aaa';
+	}
 
-				const newToken = this.jwtService.sign({
-					count: data.count + 1
-				});
-				response.setHeader('token', newToken);
-				return data.count + 1;
-			} catch (e) {
-				console.log(e);
-				throw new UnauthorizedException();
-			}
-		} else {
-			const newToken = this.jwtService.sign({
-				count: 1
-			});
-
-			response.setHeader('token', newToken);
-			return 1;
-		}
+	@Get('bbb')
+	@UseGuards(LoginGuard)
+	bbb() {
+		return 'bbb';
 	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,37 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { User } from './user/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 
 @Module({
 	imports: [
+		TypeOrmModule.forRoot({
+			type: 'mysql',
+			host: 'localhost',
+			port: 3306,
+			username: 'root',
+			password: 'Zhangrui@092',
+			database: 'login_test',
+			synchronize: true,
+			logging: true,
+			entities: [User],
+			poolSize: 10,
+			connectorPackage: 'mysql2',
+			extra: {
+				authPlugin: 'sha256_password'
+			}
+		}),
 		JwtModule.register({
+			global: true,
 			secret: 'aioloszhr',
 			signOptions: {
 				expiresIn: '7d'
 			}
-		})
+		}),
+		UserModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
